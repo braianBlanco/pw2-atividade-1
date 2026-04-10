6b4e6aee5c7162aeadc6d83013d2d7e007f8895f
nome1-nome2 <nome1-nome2@example.com>
2026-04-10 19:46:26 -0300

    Alteracao de classes relacionadas a receita

---
+++ src/Avaliacao.java
@@ -0,0 +1,9 @@
+import java.time.LocalTime;
+
+public class Avaliacao {
+    float estrela;
+    public String comentario;
+    LocalTime data;
+    public Usuario usuario;
+    boolean aprovacao;
+}
---
+++ src/ReceitaDeBolo.java
@@ -0,0 +1,10 @@
+import java.time.LocalTime;
+
+public class ReceitaDeBolo {
+    String nome;
+    LocalTime tempoPreparo;
+    int porcoes;
+    public Avaliacao avaliacao;
+    public Usuario confeiteiro;
+    public double peso;
+}
