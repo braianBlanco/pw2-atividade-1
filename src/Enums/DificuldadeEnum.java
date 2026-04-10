e2fad5d143b3fb4665e681fc0ee6574686885d5f
nome1-nome2 <nome1-nome2@example.com>
2026-04-10 20:01:33 -0300

    Alteracao de classes relacionadas ao exercício físico

---
+++ src/Enums/DificuldadeEnum.java
@@ -0,0 +1,5 @@
+package Enums;
+
+public enum DificuldadeEnum {
+    FACIL, MEDIO, DIFICIL
+}
---
+++ src/Exercicio.java
@@ -0,0 +1,10 @@
+import Enums.DificuldadeEnum;
+
+public class Exercicio {
+    public String nome;
+    public String grupoMuscular;
+    public int repeticoes;
+    public int series;
+    public double carga;
+    public DificuldadeEnum dificulade;
+}
